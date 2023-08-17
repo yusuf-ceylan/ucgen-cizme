d44d006153ed57bff3885b93dc9295d83dacca4b
Yusuf CEYLAN <yusuf.ceylann@outlook.com.tr>
2023-08-17 09:26:34 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ucgen_cizme
+{
+    public class TriangleDrawer
+    {
+        public static void DrawTriangle(int size)
+        {
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Üçgen boyutu: ");
+            int size = int.Parse(Console.ReadLine());
+
+            TriangleDrawer.DrawTriangle(size);
+        }
+    }
+}
